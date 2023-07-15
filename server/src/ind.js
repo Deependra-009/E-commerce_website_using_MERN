4beee9beda6ba89b310b141b39b35cefb971ff40
Deependra Trivedi <72593207+Deependra-009@users.noreply.github.com>
2023-07-15 13:35:12 +0530

    Update ind.js

--- server/src/ind.js
+++ server/src/ind.js
@@ -1,4 +1,4 @@
 const fast2sms = require('fast-two-sms')
 const YOUR_API_KEY='f2aSYIwcoHCDjyeZrp4F1kvBT8EhAXmzUJRWLN5l0sxg67t9QKilG9KHdMhFk8QLvXVZmrwcWUPTya1O';
-var options = {authorization : YOUR_API_KEY , message : 'deepu' ,  numbers : ['8433355000']} 
+var options = {authorization : YOUR_API_KEY , message : 'deepu' ,  numbers : ['PHONE_NUMBER']} 
 fast2sms.sendMessage(options)
